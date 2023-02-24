debc6c11b108c38075e873ae5f09650572c46bac
Brian Ingles <github@emeraldwalk.com>
2023-02-24 09:02:08 -0600

    Initial page layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+import React from 'react'
+
+export interface LayoutProps extends React.PropsWithChildren {}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <div>
+    <header>
+      <Link href="/">🏡</Link>
+    </header>
+    <main>{children}</main>
+    <footer>&copy; 2023 emeraldwalk</footer>
+  </div>
+)
+
+export default Layout
